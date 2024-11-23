ff0c50cfb0537316229859a482834250a363e50f
husscodez <huss.healthadvisor@gmail.com>
2024-11-22 16:49:02 -0800

    Update stripeWebhook.js

--- api/stripeWebhook.js
+++ api/stripeWebhook.js
@@ -1,15 +1,14 @@
 export default async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Allow GET and POST requests
+
     if (req.method === 'GET') {
-        // Respond to GET requests (useful for testing)
         res.status(200).send('Webhook endpoint is live!');
     } else if (req.method === 'POST') {
-        // Respond to POST requests (Stripe will send events here)
         console.log('Received Stripe event:', req.body);
-
-        // Acknowledge receipt of the webhook event
         res.status(200).send({ received: true });
     } else {
-        // Handle unsupported HTTP methods
         res.status(405).send('Method Not Allowed');
     }
 };
